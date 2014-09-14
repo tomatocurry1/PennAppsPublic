046c6c429d697b06c22a49acffd2126f317cbb47
Nikhil Sharma <flyamonkey@gmail.com>
2014-09-13 23:33:55 -0400

    Hid action bar on main menu

--- src/com/jonathon_vogel/pennapps/MainActivity.java
+++ src/com/jonathon_vogel/pennapps/MainActivity.java
@@ -44,6 +44,7 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		getActionBar().hide();
 		
 		handler = new Handler();
 		setContentView(R.layout.activity_main);
